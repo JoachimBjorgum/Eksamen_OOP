0c7da8455eebfb24f322ce017994142f81838894
Joachim Bjorgum <Joachim.Bjorgum@gmail.com>
2022-05-30 10:14:20 +0200

    Synching project to Github

---
+++ src/Oop.java
@@ -0,0 +1,16 @@
+public class Oop {
+    public static void main(String args[]) {
+        race();
+        shape3d();
+        chess();
+    }
+    private static void race() {
+        System.out.println("\nSimulated race runners:");
+    }
+    private static void shape3d() {
+        System.out.println("\nShape3d:");
+    }
+    private static void chess() {
+        System.out.println("\nChess:");
+    }
+}
---
+++ src/Race.java
@@ -0,0 +1,29 @@
+import java.text.DecimalFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Race {
+    int startTime;
+    int endTime;
+    int timeUsed;
+
+    public void start(String runners, String ageGroup, int distance) {
+
+    }
+    public void start(String runners, String ageGroup, int distance, int offset) {
+
+    }
+
+public static class Group {
+
+    public void simulate() {
+        System.out.println("Start: " + ", End: " + ", Used: " + ", Distance: " +
+                           ", Speed: " + ", Speed: " + ", Age:" + ", Name: " + ", Age Group: ");
+    }
+}
+
+public static class Runner {
+
+}
+
+}
